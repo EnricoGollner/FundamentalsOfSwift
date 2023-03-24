e6d6126559c960cc13572c19ebeeba76ed3ce917
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-03-23 21:49:46 -0300

    Fundamentals and revisitating some points

---
+++ swiftPoints.playground/Contents.swift
@@ -0,0 +1,53 @@
+import Cocoa
+
+// Tuple:
+var tp = ("Swift", 5, true)
+print(tp.0)
+
+var tpMelhor = (name: "Swift", version: 5, best: true)
+print(tpMelhor.0)
+print(tpMelhor.name)
+
+
+// typealias
+
+struct MovieModel {
+    let title: String
+    let director: String
+    let countLikes: Int
+}
+
+// typealias creates a new name for an existing type.
+typealias TVModel = MovieModel
+
+let movie = MovieModel(title: "Star Wars", director: "George Lucas", countLikes: 1_000_000)
+
+let tv = TVModel(title: "The Mandalorian", director: "A lot", countLikes: 999_000)
+
+
+typealias Thing = [String: Any]
+var stuff: Thing;
+print(type(of: stuff))
+
+
+// superclass (parent class)
+class Dog {
+    let name: String
+    let breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+// child class:
+class Poodle: Dog{
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+}
+
+let poodle = Poodle(name: "Luna")
+print(poodle.name)
+print(poodle.breed)
+
